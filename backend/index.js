277ccfdf633e7e3844db0eb9fa220ef2ca877d9a
shourjoac <131592449+shourjoac@users.noreply.github.com>
2024-07-20 00:06:07 +0530

    Initial_Commit
    Will be updated

---
+++ backend/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+const mainRouter = require("./routes/index")
+
+app.use('/api/v1', mainRouter)
+app.listen(3000)
