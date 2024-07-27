6a418710370154e0e1e32b5ced2770b17e6414a1
shourjoac <131592449+shourjoac@users.noreply.github.com>
2024-07-28 01:35:08 +0530

    update
    update

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+
 const cors = require("cors");
 app.use(cors());
+
 app.use(express.json());
+
 const mainRouter = require("./routes/index")
 
 app.use('/api/v1', mainRouter)
